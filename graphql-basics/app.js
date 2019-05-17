1df6d725448e5c9edee6f7ea5d1ff40d3c4fcdb7
OneJChristensen <joshua@thedigitaldesign.com>
2019-05-16 21:38:01 -0600

    Added in more complex queries and resolvers to support them. It's all still very basic, but so cool!

--- graphql-basics/app.js
+++ graphql-basics/app.js
@@ -1,19 +1,99 @@
 const express = require('express')
 const { ApolloServer, gql } = require('apollo-server-express')
 
+const uuidv4 = require('uuid/v4')
+
+/* 
+    Scalar types:
+        - String
+        - Boolean
+        - Int, Float
+        - ID (Must be uppercase)
+*/
+
 // Schema
 const typeDefs = gql`
     type Query {
-        hello: String!
+        me: User!
+        products: [Product!]!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+    }
+
+    type Product {
+        id: ID!
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        hello: () => {
-            return 'Hello GraphQL Query!'
-        }
+        me: () => {
+            return {
+                id: () => {
+                    return uuidv4()
+                },
+                name: () => {
+                    return 'Joshua Christensen'
+                },
+                email: () => {
+                    return 'joshua@thedigitaldesign.com'
+                }
+            }
+        },
+        products: () => {
+            return [
+                {
+                    id: () => {
+                        return uuidv4()
+                    },
+                    title: () => {
+                        return 'Asus Router'
+                    },
+                    price: () => {
+                        return 46.99
+                    },
+                    releaseYear: () => {
+                        return 1999
+                    },
+                    rating: () => {
+                        return 4
+                    },
+                    inStock: () => {
+                        return false
+                    }
+                },
+                {
+                    id: () => {
+                        return uuidv4()
+                    },
+                    title: () => {
+                        return 'GraphQL for Zombies'
+                    },
+                    price: () => {
+                        return 46.99
+                    },
+                    releaseYear: () => {
+                        return 2019
+                    },
+                    rating: () => {
+                        return 5
+                    },
+                    inStock: () => {
+                        return true
+                    }
+                }
+            ]
+        },
     }
 }
 
@@ -22,6 +102,6 @@ const app = express()
 
 server.applyMiddleware({ app })
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+app.listen({ port: 9000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:9000${server.graphqlPath}`)
 )
